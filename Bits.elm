aca2be182e067202572cceecf5ecbe9c87340517
Rohith Daka <rohith.daka@gmail.com>
2016-04-03 20:23:33 -0700

    Finished building a Bit block

--- Bits.elm
+++ Bits.elm
@@ -2,7 +2,8 @@ module Bits where
 
 import Basics exposing (..)
 import Color exposing (Color)
-import Text exposing (fromString)
+-- Not exposing Text functions as they may conflict with Graphics functions
+import Text
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
@@ -10,17 +11,22 @@ type alias Bit ={   value : Int
                 ,   color : Color
                 }
 
-{- The length of the edges of each block -}                 
+{- The length of the edges of each Bit block -}                 
 size : Float
-size = 25
+size = 40
 
-{- Given a Block, converts it to a Form. -}       
+{- Given a Bit type, converts it to a Form. -}       
 toForm : Bit -> Form
 toForm bit = 
     let shape = square size
-        border = outlined (solid Color.gray) shape
-        bitDisplay = text (fromString (toString bit.value))
+        border = outlined (solid Color.darkCharcoal) shape
+        bitDisplay = toString bit.value 
+                    |> Text.fromString 
+                    |> Text.color Color.green
+                    |> Text.monospace
+                    |> Text.height (0.8*size)
+                    |> text 
     in  group [filled bit.color shape, border, bitDisplay]
 
 main : Element     
-main = collage 400 400 [toForm (Bit 0 Color.red)]
+main = collage 400 400 [toForm (Bit 0 Color.black)]
