edd32db25a8fca94c492d996dfc2730044241ecf
Rohith Daka <rohith.daka@gmail.com>
2016-04-04 00:19:47 -0700

    Changed Bit Model to just int as color is linked to the bit value and shouldn't be allowed to change independent of the bit value

--- Bits.elm
+++ Bits.elm
@@ -7,26 +7,38 @@ import Text
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
-type alias Bit ={   value : Int
-                ,   color : Color
-                }
+type alias Bit = Int
 
 {- The length of the edges of each Bit block -}                 
 size : Float
 size = 40
 
+colorOf : Bit -> Color
+colorOf bit=
+    case bit of 
+        0 -> Color.black
+        1 -> Color.white
+        _ -> Color.red
+
+
 {- Given a Bit type, converts it to a Form. -}       
 toForm : Bit -> Form
 toForm bit = 
     let shape = square size
-        border = outlined (solid Color.darkCharcoal) shape
-        bitDisplay = toString bit.value 
+        border = outlined (solid Color.darkCharcoal) shape 
+        bitDisplay = toString bit
                     |> Text.fromString 
                     |> Text.color Color.green
                     |> Text.monospace
                     |> Text.height (0.8*size)
                     |> text 
-    in  group [filled bit.color shape, border, bitDisplay]
+    in  group [filled (colorOf bit) shape, border, bitDisplay]
+
+
+bitToggle : Bit -> Bit
+bitToggle bit = 
+    (bit + 1) % 2
+
 
 main : Element     
-main = collage 400 400 [toForm (Bit 0 Color.black)]
+main = collage 400 400 [toForm (4)]
