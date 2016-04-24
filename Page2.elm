e8904d8403b6b407e6004edb11709cec0d966511
Rohith Daka <rohith.daka@gmail.com>
2016-04-24 12:22:47 -0700

    Created page2

--- Page2.elm
+++ Page2.elm
@@ -1,10 +1,52 @@
-import Packet
-import StartApp.Simple exposing (start)
+import Packet 
+import Header 
+import Html
+import StartApp.Simple exposing(start)
+
+type alias AppModel = 
+    { headerModel: Header.Model
+    , packetModel: Packet.Model 
+    }
+
+initialModel: AppModel
+initialModel = 
+    { headerModel = Header.initialModel
+    , packetModel = (Packet.emptyPacket)
+    }
+
+type Action 
+    = PacketAction Packet.Action
 
 
-main =
-  start
-    { model = Packet.emptyPacket
-    , update = Packet.update
-    , view = Packet.view
+view: Signal.Address Action -> AppModel -> Html.Html
+view address model =
+    Html.div []
+        [ Html.header 
+            [] 
+            [Header.view model.headerModel]
+        , Html.body
+            []
+            [ Html.p 
+                []
+                [ Html.text "This is a packet\n" ]
+            , Packet.view (Signal.forwardTo address PacketAction) model.packetModel
+            ]
+        ]
+
+
+update: Action -> AppModel -> AppModel
+update action model =
+    case action of
+        PacketAction subAction ->
+            let 
+                updatedPacketModel = 
+                    Packet.update subAction model.packetModel
+            in
+                {model | packetModel = updatedPacketModel}
+
+main = 
+    start { 
+        model = initialModel
+    ,   view = view
+    ,   update = update 
     }
